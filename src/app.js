945119b97f830cdf5577a0c64d4348f621c34ad5
jayala2022 <jessica.ayala2019@outlook.com>
2022-08-12 11:58:42 -0700

    Showing the current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
+function displayTemperature(response) {
+  console.log(response.data);
 
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let temperatureElement = document.querySelector("#temperature");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  humidityElement.innerHTML = Math.round(response.data.main.humidity);
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "f6354936229195f9aa501c092c5831ca";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New%20York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
